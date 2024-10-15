40ef5d83f371f7f356f540e721b11d5c426b971c
alihuzaifadeveloper <ali.huzaifa@nebrascorp.com>
2024-10-15 10:53:27 +0500

    Update BaseComponent.tsx

--- app/components/BaseComponent.tsx
+++ app/components/BaseComponent.tsx
@@ -1,15 +1,52 @@
-// components/BaseComponent.tsx
-import React, { Component } from 'react'
+"use client";
+import React, { Component } from "react";
 
-// The generic base component class with types for Props and State
-export default class BaseComponent<P = {}, S = {}> extends Component<P, S> {
-  constructor(props: P) {
-    super(props)
+interface Task {
+  id: number;
+  title: string;
+}
+
+interface BaseState {
+  tasks: Task[];
+  newTask: string;
+}
+
+interface BaseProps {}
+
+export default class BaseComponent extends Component<BaseProps, BaseState> {
+  constructor(props: BaseProps) {
+    super(props);
+    this.state = {
+      tasks: [],
+      newTask: "",
+    };
   }
 
-  // Common method to handle input changes for any text input
-  handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = e.target
-    this.setState({ [name]: value } as unknown as S)
+  addTodo = () => {
+    const { newTask, tasks } = this.state;
+    if (newTask.trim() !== "") {
+      const newTaskItem: Task = {
+        id: tasks.length > 0 ? tasks[tasks.length - 1].id + 1 : 1,
+        title: newTask,
+      };
+      this.setState({
+        tasks: [...tasks, newTaskItem],
+        newTask: "",
+      });
+    }
+  };
+
+  deleteTodo = (id: number) => {
+    this.setState((prevState) => ({
+      tasks: prevState.tasks.filter((task) => task.id !== id),
+    }));
+  };
+
+  handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({ newTask: event.target.value });
+  };
+
+  render(): React.JSX.Element | null {
+    return null; // No rendering logic here; child components can implement their own
   }
 }
